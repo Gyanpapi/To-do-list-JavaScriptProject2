595bc637998e85471d86cbede99855c63ec67f86
Gyanpapi <122615523+Gyanpapi@users.noreply.github.com>
2025-04-16 23:33:17 +0600

    updated css and javascript codes

--- script.js
+++ script.js
@@ -14,10 +14,36 @@ function addItem()
         li.innerHTML=inputBox.value;
         listcontainer.appendChild(li);
         inputBox.value='';
-
+        saveData();
         const myspan=document.createElement("span");
         myspan.innerHTML="\u00d7";
         li.appendChild(myspan);
+        saveData();
     }
 
 }
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI")
+    {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN")
+    {
+        e.target.parentElement.remove();
+        saveData();
+    }
+})
+
+
+function saveData()
+{
+    localStorage.setItem("data", listcontainer.innerHTML);
+}
+function showtask()
+{
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+
+showtask();
